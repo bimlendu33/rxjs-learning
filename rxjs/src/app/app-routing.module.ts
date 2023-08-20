84c0543c9d9e0b2706a588dbe15375a04a2e8883
Bimlendu <kumar1991harsh@gmail.com>
2023-08-20 20:23:26 +0530

    few more operators added with explanation

--- rxjs/src/app/app-routing.module.ts
+++ rxjs/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+import { LandingComponent } from './landing/landing.component';
+import { ObservableComponent } from './observable/observable.component';
+import { TimerComponent } from './timer/timer.component';
+import { IntervalComponent } from './interval/interval.component';
+import { OfComponent } from './of/of.component';
+import { FromComponent } from './from/from.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent, pathMatch: 'full' },
+  { path: 'promise', component: PromiseComponent },
+  { path: 'observable', component: ObservableComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'interval', component: IntervalComponent },
+  { path: 'of', component: OfComponent },
+  { path: 'from', component: FromComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- rxjs/src/app/app.module.ts
+++ rxjs/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromiseComponent } from './promise/promise.component';
+import { HttpClientModule } from  '@angular/common/http';
+import { LandingComponent } from './landing/landing.component';
+import { ObservableComponent } from './observable/observable.component';
+import { TimerComponent } from './timer/timer.component';
+import { IntervalComponent } from './interval/interval.component';
+import { OfComponent } from './of/of.component';
+import { FromComponent } from './from/from.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PromiseComponent,
+    LandingComponent,
+    ObservableComponent,
+    TimerComponent,
+    IntervalComponent,
+    OfComponent,
+    FromComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
