7c8ea8e969296c8db9c6ac96cf188a774654ffa3
Bimlendu <kumar1991harsh@gmail.com>
2023-08-21 09:45:32 +0530

    added comments

--- rxjs/src/app/interval/interval.component.ts
+++ rxjs/src/app/interval/interval.component.ts
@@ -14,6 +14,7 @@ export class IntervalComponent implements OnInit, OnDestroy {
     // interval operator is similar to setTimeout, only difference is it emits data from 0 to infinite in every N seconds
     // N seconds is passed as paramter to interval operator and data will be emitted in every N seconds
     // with tap we collect emitted data by interval and perform additional operations like here we unsubsribed from interval subscription
+    // if we did not want to unsubscribe then we had simply subscribed to collect data
     this.intervalSubs = interval(1000)
       .pipe(
         tap((item) => {
